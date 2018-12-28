97691a45fa0ee99ed9028febd1fb494b0721d774
LinuxparaChen <13073134757@163.com>
2018-12-28 17:44:37 +0800

    抽取发布代码

--- EdgeMvp-Annotation/src/main/java/com/linuxpara/edgemvp_annotation/proxy/EdgeMvpPresenter.java
+++ EdgeMvp-Annotation/src/main/java/com/linuxpara/edgemvp_annotation/proxy/EdgeMvpPresenter.java
@@ -11,37 +11,41 @@ import java.lang.ref.WeakReference;
  * *************************************************************
  * Description: presenter代理类，的顶级父类
  */
-public abstract class EdgeMvpPresenter<V,P> {
+public abstract class EdgeMvpPresenter<V, P> {
 
     private final WeakReference<P> mWRPresenter;
     private EdgeMvpView<V> mViewProxy;
 
-    public EdgeMvpPresenter(V view, P presenter){
+    public EdgeMvpPresenter(V view, P presenter) {
         mViewProxy = createViewProxy(view);
         mWRPresenter = new WeakReference<>(presenter);
     }
 
     /**
      * 创建view层的代理类
-     * @param view
-     * @return
+     *
+     * @param view 具体的view
+     * @return view的代理类
      */
     protected abstract EdgeMvpView<V> createViewProxy(V view);
 
     /**
      * 获取view层的代理类
-     * @return
+     *
+     * @return 获取view的代理类
      */
     public EdgeMvpView<V> getViewProxy() {
         return mViewProxy;
     }
 
     /**
-     * 获取persenter
-     * @return
+     * 获取presenter
+     *
+     * @return 具体的presenter
+     * @throws Exception 弱引用被回收异常
      */
     public P getPresenter() throws Exception {
-        if (mWRPresenter != null && mWRPresenter.get() != null){
+        if (mWRPresenter != null && mWRPresenter.get() != null) {
             return mWRPresenter.get();
         }
         throw new Exception("presenter 引用已经被GC回收！");
